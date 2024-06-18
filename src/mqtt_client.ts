ebdee6d2f81c259549334f6cf87c2bcb8685e360
Erich Querner <querner@interactive-scape.com>
2024-06-18 09:37:33 +0200

    implemented mqtt abstraction layer

--- src/mqtt_client.ts
+++ src/mqtt_client.ts
@@ -1,13 +1,19 @@
-import mqtt from "mqtt";
+import mqtt, { IClientSubscribeOptions } from "mqtt";
 
 interface Message {
     topic: string,
-    qos: number,
+    qos: QoS,
     payload: Buffer | string,
     retained: boolean,
 }
 
-type Callback = () => void;
+type Callback = (payload?: string | Buffer) => void;
+
+enum QoS {
+    Zero = 0,
+    One = 1,
+    Two = 2,
+}
 
 export class MqttClient {
 
@@ -15,14 +21,15 @@ export class MqttClient {
     private client: mqtt.MqttClient;
     private isOpen: boolean;
     private onConnectedCallbacks: Callback[];
-    private onMessageDict: Record<string, Callback>;
+    private onDisconnectedCallbacks: Callback[];
+    private onMessageDict: Map<string, Callback>;
     private messageQueue: Message[];
     private subscriptionQueue: Callback[];
 
-
     constructor(brokerUrl: string, clientId: string, username: string = "", password: string = "") {
         this.onConnectedCallbacks = [];
-        this.onMessageDict = {};
+        this.onDisconnectedCallbacks = [];
+        this.onMessageDict = new Map<string, Callback>();
         this.messageQueue = [];
         this.subscriptionQueue = [];
 
@@ -42,9 +49,9 @@ export class MqttClient {
         this.client.connect();
     }
 
-    public send(topic: string, message: string | Buffer, qos: number = 0, retained: boolean = false) {
+    public send(topic: string, message: string | Buffer, qos: QoS, retained: boolean = false) {
         if (this.isOpen) {
-            this.client.publish("sxm/" + topic, message, { retain: retained });
+            this.client.publish("sxm/" + topic, message, { qos: qos, retain: retained });
         } else {
             const msg: Message = {
                 topic: topic,
@@ -58,32 +65,60 @@ export class MqttClient {
 
     public subscribe(topic: string, onMessageCallback: Callback) {
         if (this.isOpen) {
-            this.client.subscribe(topic, (error) => {
+            const options: IClientSubscribeOptions = { qos: QoS.One };
+            this.client.subscribe(topic, options, (error) => {
                 if (error) {
                     console.log(`Could not subscribe to topic: ${topic} -> ${error.message}`);
                 } else {
-                    this.onMessageDict[topic] = onMessageCallback;
+                    this.onMessageDict.set(topic, onMessageCallback);
                     console.log(`Subscribed to topic: ${topic}`);
                 }
             })
         }
     }
 
+    public registerOnConnected(callback: Callback) {
+        this.onConnectedCallbacks.push(callback);
+    }
+
+    public registerOnDisconnected(callback: Callback) {
+        this.onDisconnectedCallbacks.push(callback);
+    }
+
     private onConnect(connack: mqtt.IConnackPacket): void {
         console.log("MQTT Client connected.");
         this.triggerCallbacks(this.onConnectedCallbacks);
         this.isOpen = true;
+        this.messageQueue.forEach(message => {
+            this.send(message.topic, message.payload, message.qos, message.retained);
+        });
+        this.messageQueue = [];
 
     }
     private triggerCallbacks(callbacks: Callback[]) {
         callbacks.forEach(callback => callback());
     }
+
     private onMessageArrived(topic: string, message: Buffer, packet: mqtt.IPublishPacket): void {
-        const msg: Message = { topic: packet.topic, qos: packet.qos, payload: packet.payload, retained: packet.retain };
+        if (this.onMessageDict.has(topic)) {
+            const callback = this.onMessageDict.get(topic);
+            if (callback != null) {
+                callback(packet.payload);
+            } else {
+                console.log(`Received message ${message}, but no handler was given.`);
+            }
+        } else {
+            console.log(`Received message for topic ${topic}, but no listeners for the topic were found.`);
+        }
     }
+
     private onDisconnect(response: mqtt.IDisconnectPacket): void {
-        throw new Error("Method not implemented.");
+        if (response.reasonCode !== 0) {
+            console.log(`Connection lost because: ${response.properties?.reasonString}`);
+        } else {
+            this.triggerCallbacks(this.onDisconnectedCallbacks);
+            console.log("Connection to the MQTT broker was closed.");
+        }
+        this.isOpen = false;
     }
-
-
 }
